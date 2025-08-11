a0f6722825c7ad8c20084d23cbdedd48aa38f99d
Alexaner Yarovinsky <48382982+AlexanderY88@users.noreply.github.com>
2025-08-11 20:53:55 +0300

    typescript lesson 21

--- lesson21/script.js
+++ lesson21/script.js
@@ -1,2 +1,134 @@
 var myName = "Alex";
 console.log(myName);
+var age = 92;
+var isLoggedIn = false;
+console.log(age, isLoggedIn);
+// more than one type in a variable
+var price = 100;
+var data = false;
+// super types
+// any: can be any type like in simple js 
+var animal = [];
+animal = 5;
+animal = true;
+animal = "Mizzy";
+animal = [1, 2, 3];
+console.log(animal);
+// let value: any = 80;
+// value.toUpperCase(); // error after compiling - in the web page
+// let value2: unknown = 80;
+// value2.toUpperCase(); // error in the time of writing code - can be useful to avoid a bug
+// casting
+var id = "1234"; // => console.log(id.length);  will be error
+console.log(id.length); // => (id as string).length = casting unknown to string, than we can use string methods like length without error 
+// arrays
+var names = ["Alex", "Bob", "Charlie"];
+console.log(names);
+// names.push(7);  => error because 7 is not a string
+// if crate an array like this: let names : string[]  | number[] .... => will not been error when:  names.push(7);
+var ages = [10, 20, 30]; // another way to create array,calls: generics
+// function check even or odd
+function checkEven(num) {
+    if (num % 2 === 0)
+        return true;
+    return false;
+}
+console.log(checkEven(8));
+console.log(checkEven(9));
+// function without parameters ir return
+function printMSG(msg) {
+    console.log(msg);
+}
+;
+printMSG("Hello, TypeScript!");
+function welcome(username) {
+    if (username === void 0) { username = "Margol"; }
+    alert("Welcome, ".concat(username, "!"));
+}
+// welcome("Alex");
+// JS object
+var user = {
+    id: "123",
+    name: "Margol",
+    age: 30,
+    isLoggedIn: true
+};
+// TS object
+var user2 = {
+    id: "123",
+    username: "Margol",
+    age: 30,
+    isLoggedIn: true
+};
+var user3 = {
+    id: "456",
+    username: "Nissim",
+    age: 70,
+    isLoggedIn: false
+};
+// array of users
+var users = [
+    {
+        id: "12",
+        username: "Sarit",
+        age: 42,
+        isLoggedIn: false,
+    },
+    {
+        id: "13",
+        username: "Zohar",
+        isLoggedIn: false,
+    },
+];
+console.log(users);
+// oop - object oriented programming 
+// enum - let create constant variables that cannot be changed
+var Directions;
+(function (Directions) {
+    Directions[Directions["North"] = 0] = "North";
+    Directions[Directions["East"] = 1] = "East";
+    Directions[Directions["West"] = 2] = "West";
+    Directions[Directions["South"] = 3] = "South";
+})(Directions || (Directions = {}));
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+    StatusCodes[StatusCodes["Created"] = 201] = "Created";
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+    StatusCodes[StatusCodes["InternalServerError"] = 500] = "InternalServerError";
+})(StatusCodes || (StatusCodes = {}));
+console.log(StatusCodes.Created);
+var employees = [
+    {
+        id: "1",
+        name: "Rivka",
+        age: 30,
+        salary: 50000,
+        department: "HR"
+    },
+    {
+        id: "2",
+        name: "Avi",
+        age: 48,
+        salary: 37000,
+        department: "Phone assistant"
+    },
+    {
+        id: "3",
+        name: "Margol",
+        salary: 90000,
+        department: "Director"
+    }
+];
+// my with copilot suggestion
+// function showUsers(array: Employee[]): void {
+//     for (let i : number = 0; i < array.length; i++) {
+//         console.log(`${array[i].name}, ${array[i].salary}`);
+//     }
+// }
+// showUsers(employees);
+for (var _i = 0, employees_1 = employees; _i < employees_1.length; _i++) {
+    var employee = employees_1[_i];
+    console.log(employee.name, employee.salary);
+}
--- lesson21/script.ts
+++ lesson21/script.ts
@@ -1,2 +1,181 @@
 let myName : string = "Alex";
 console.log(myName);
+
+let age: number = 92;
+let isLoggedIn: boolean = false;
+console.log(age, isLoggedIn);
+
+// more than one type in a variable
+let price : number | string = 100;
+// it's possible to create new variable type and set more than one variable type to it
+type alex = string | boolean;
+let data : alex = false;
+
+// super types
+// any: can be any type like in simple js 
+let animal : any = [];
+animal = 5;
+animal = true;
+animal = "Mizzy";
+animal = [1, 2, 3];
+console.log(animal);
+
+
+// let value: any = 80;
+// value.toUpperCase(); // error after compiling - in the web page
+
+// let value2: unknown = 80;
+// value2.toUpperCase(); // error in the time of writing code - can be useful to avoid a bug
+
+
+// casting
+let id: unknown = "1234"; // => console.log(id.length);  will be error
+
+console.log((id as string).length); // => (id as string).length = casting unknown to string, than we can use string methods like length without error 
+
+
+// arrays
+let names : string[] = ["Alex", "Bob", "Charlie"];
+console.log(names);
+// names.push(7);  => error because 7 is not a string
+// if crate an array like this: let names : string[]  | number[] .... => will not been error when:  names.push(7);
+
+let ages: Array<number> = [10, 20, 30]; // another way to create array,calls: generics
+
+// function check even or odd
+function checkEven(num: number) : boolean {
+   if (num % 2 === 0) return true;
+   return false;
+}
+
+console.log(checkEven(8));
+console.log(checkEven(9));
+
+// function without parameters ir return
+function printMSG(msg: string): void {
+    console.log(msg);
+};
+printMSG("Hello, TypeScript!");
+
+
+function welcome(username: string = "Margol"): void {
+    alert(`Welcome, ${username}!`);
+}
+
+// welcome("Alex");
+
+// JS object
+let user = {
+    id: "123",
+    name: "Margol",
+    age: 30,
+    isLoggedIn: true
+};
+// TS object
+let user2 : {id: string, username: string, age?: number, isLoggedIn: boolean} = {
+    id: "123",
+    username: "Margol",
+    age: 30,
+    isLoggedIn: true
+};
+// in the object in typescript must declare the variables 
+// age?: number   => means that age is not mandatory, for literal objects when the same constructor in use at more than one place
+// design patterns is: interface that means the same code is in use at a lot of places, when it's possible
+
+
+// interface for TS (not exist in js ), or Literal object
+interface User {
+    id:string;
+    username: string;
+    age?: number;
+    isLoggedIn: boolean;
+}
+
+let user3 : User = {
+    id: "456",
+    username: "Nissim",
+    age: 70,
+    isLoggedIn: false
+}
+
+// array of users
+let users: User[] = [
+     {
+    id: "12",
+    username: "Sarit",
+    age: 42,
+    isLoggedIn: false,
+  },
+  {
+    id: "13",
+    username: "Zohar",
+    isLoggedIn: false,
+  },
+];
+
+console.log(users);
+
+// oop - object oriented programming 
+
+
+// enum - let create constant variables that cannot be changed
+enum Directions {
+    North,
+    East,
+    West,
+    South
+} 
+
+enum StatusCodes {
+    Success = 200,
+    Created = 201,
+    NotFound = 404,
+    BadRequest = 400,
+    InternalServerError = 500
+}
+
+console.log(StatusCodes.Created);
+
+// Exercise 1
+interface Employee {
+    id: string;
+    name: string;
+    age?: number;
+    salary: number;
+    department: string;
+}
+
+let employees: Employee[] = [
+    {
+        id: "1",
+        name: "Rivka",
+        age: 30,
+        salary: 50000,
+        department: "HR"
+    },
+    {
+        id: "2",
+        name: "Avi",
+        age: 48,
+        salary: 37000,
+        department: "Phone assistant"
+    },
+    {
+        id: "3",
+        name: "Margol",
+        salary: 90000,
+        department: "Director"}
+];
+
+// my with copilot suggestion
+
+// function showUsers(array: Employee[]): void {
+//     for (let i : number = 0; i < array.length; i++) {
+//         console.log(`${array[i].name}, ${array[i].salary}`);
+//     }
+// }
+
+// showUsers(employees);
+
+for(let employee of employees) console.log(employee.name, employee.salary);
+
